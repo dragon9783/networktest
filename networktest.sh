e0392605ba655f156935e66713f52931eb88a3eb
dragon9783 <dragon9783@126.com>
2016-01-25 07:48:38 +0800

    Update networktest.sh

--- networktest.sh
+++ networktest.sh
@@ -6,15 +6,18 @@ fi
 
 while true
 do
-  if ! ping -c 1 -W 2 github.com  &> /dev/null; then
+  ping -c 1 -W 2 github.com  &> /dev/null
+  if [ $? -ne 0 ] ; then
     echo `date "+%Y-%m-%d %H:%M:%S"` "ping github.com  ->  failed" >> /var/log/networktest/networktest.log
     logger -it remote -p local6.error -n 172.31.12.85 -P 514 -d "$(hostname) $(date +'%Y-%m-%d %H:%M:%S') ping github.com  ->  failed"
   fi
-  if ! nc -v -w 2 -z -i 1 github.com 443  &> /dev/null; then
+  nc -v -w 2 -z -i 1 github.com 443  &> /dev/null
+  if [ $? -ne 0 ] ; then
     echo `date "+%Y-%m-%d %H:%M:%S"` "nc github.com port 443  ->  failed" >> /var/log/networktest/networktest.log
     logger -it remote -p local6.error -n 172.31.12.85 -P 514 -d "$(hostname) $(date +'%Y-%m-%d %H:%M:%S') nc github.com port 443  ->  failed"
   fi
-  if ! nc -v -w 2 -z -i 1 docker.com 443  &> /dev/null; then
+  nc -v -w 2 -z -i 1 docker.com 443  &> /dev/null
+  if [ $? -ne 0 ] ; then
     echo `date "+%Y-%m-%d %H:%M:%S"` "nc docker.com port 443 ->  failed" >> /var/log/networktest/networktest.log
     logger -it remote -p local6.error -n 172.31.12.85 -P 514 -d "$(hostname) $(date +'%Y-%m-%d %H:%M:%S') nc docker.com port 443  ->  failed"
   fi
